84a42e337640b79c478cbeb3691cfd7f163f243e
oussama-bht-proclus <oussama.boumhaout@proclus-group.com>
2024-05-21 15:27:47 +0100

    test orchestrate Download->Upload

--- IntegrationContent/IntegrationArtefacts/com.nmbs.if.GLB.RDMT2VBX.I0010.TACO_DERIVED_TACO.Read/src/main/resources/script/test payload.groovy
+++
@@ -1,7 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-def Message processData(Message message) {
-    def body = message.getBody(String)
-    message.setProperty('EmptyPayload', body == null || body.isEmpty())
-    return message
-}
